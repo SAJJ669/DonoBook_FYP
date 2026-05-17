4fe8f7cec70b713fff53487f3163d31da990e1ed
sajj669 <chsajjawalali@gmail.com>
2026-05-18 01:59:17 +0500

    Added PWA:
    - Usedvite-plugin-pwa so our web can act as a mobile app
    
    Modified vite.config.ts and manifest.json to handle PWA
    
    Added two png images for out app logo and splash screen
    
    Added a few lines of code in main.tsx so we can install app through browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -8,10 +9,54 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate', // Automatically refreshes the app when updates are deployed
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+    manifest: {
+      name: 'DonoBook - Book Exchange & Donation',
+      short_name: 'DonoBook',
+      description: 'Donate and exchange books easily in your community',
+      theme_color: '#4304c2', // Change this to your app's primary brand color
+      background_color: '#ffffff',
+      display: 'standalone', // Makes it look like a native app without browser URL bars
+      orientation: 'portrait',
+      start_url: '/',
+      icons: [
+        {
+          src: '/logo-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/logo-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: '/logo-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable' // Crucial for nice circular/squared icons on Android
+        },
+      ]
+    },
+    workbox: {
+      cleanupOutdatedCaches: true
+    },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    allowedHosts: [
+      'minds-hidden-item-rom.trycloudflare.com',
+      'reviews-firewall-preview-logo.trycloudflare.com',
+      'tribunal-rapids-which-deaths.trycloudflare.com'
+    ]
+  }
 }));
